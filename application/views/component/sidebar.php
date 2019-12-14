e096ceb9605612b3ecdebf9464e517525571fa29
Fitrah Hidayat <huntu153_remot@yahoo.com>
2019-12-14 21:30:07 +0700

    ganti menu pake statis

--- application/views/component/sidebar.php
+++ application/views/component/sidebar.php
@@ -1,14 +1,28 @@
+<?php 
+$data['uri'] = $this->uri->segment(1);
+?>
 <div class="sidebar-category sidebar-category-visible">
 						<div class="category-content no-padding">
-							<ul class="navigation navigation-main navigation-accordion">
+						<ul class="navigation navigation-main navigation-accordion">
 
-								<!-- Main -->
-								
-								<?php
-								echo $this->menu->build_menu($this->session->userdata('grup'));
-								?>
-								
-								<!-- /page kits -->
+							<!-- Main -->
+
+							<li <?= ($data['uri'] == 'dashboard' ? 'class="active"' : '')?>><a href="<?=base_url();?>dashboard"><i class="icon-home7"></i><span>Beranda</span></a></li>
+							<li <?= ($data['uri'] == 'anggota' ? 'class="active"' : '')?>><a href="<?=base_url();?>anggota"><i class="icon-users"></i><span>Anggota</span></a></li>
+							<li>
+								<a href="#"><i class="icon-calendar"></i><span>Event</span></a>
+								<ul>
+									<li <?= ($data['uri'] == 'event' ? 'class="active"' : '')?>><a href="<?=base_url();?>event"><i class="icon-calendar"></i><span>List Event</span></a></li>
+									<li <?= ($data['uri'] == 'komentar' ? 'class="active"' : '')?>><a href="<?=base_url();?>komentar"><i class="icon-price-tags"></i><span>Komentar</span></a></li>
+								</ul>
+							</li>
+							
+							<li <?= ($data['uri'] == 'anggota' ? 'class="project"' : '')?>><a href="<?=base_url();?>project"><i class="icon-archive"></i><span>Proyek</span></a></li>
+							<li <?= ($data['uri'] == 'anggota' ? 'class="training"' : '')?>><a href="<?=base_url();?>training"><i class="icon-lifebuoy"></i><span>Pelatihan</span></a></li>
+							<li <?= ($data['uri'] == 'anggota' ? 'class="unggah_karya"' : '')?>><a href="<?=base_url();?>unggah_karya"><i class="icon-folder-upload"></i><span>Unggah Karya</span></a></li>
+							<li <?= ($data['uri'] == 'kategori' ? 'class="active"' : '')?>><a href="<?=base_url();?>kategori"><i class="icon-price-tags"></i><span>Kategori</span></a></li>
+															
+							<!-- /page kits -->
 
 							</ul>
 						</div>

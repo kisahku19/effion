1e5530fcfe884dc44506f92db96bd24ec5c31ec0
Fitrah Hidayat <huntu153_remot@yahoo.com>
2020-01-19 22:24:32 +0700

    ketinggalan forum

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -15,15 +15,15 @@ class Dashboard extends HSM_Conttroller
     {
        
         $data['title'] = 'Beranda';
-        $hasil = $this->dashboard_model->get_all_forum();
-        $data['graph'] = json_encode($hasil);
+        //$hasil = $this->dashboard_model->get_all_forum();
+        //$data['graph'] = json_encode($hasil);
        $data['page_js'] = 'page/dashboard/page_js';
        $data['total_anggota'] = $this->dashboard_model->jumlah_anggota();
        $data['total_event'] = $this->dashboard_model->jumlah_event();
        $data['total_project'] = $this->dashboard_model->jumlah_project();
        $data['total_training'] = $this->dashboard_model->jumlah_training();
        $data['total_karya'] = $this->dashboard_model->jumlah_karya();
-       $data['total_forum'] = $this->dashboard_model->jumlah_forum();
+       //$data['total_forum'] = $this->dashboard_model->jumlah_forum();
         $data['content'] = 'page/dashboard/dashboard_view';
         
         $this->load->view('wrapper', $data);
--- application/models/event_model.php
+++ application/models/event_model.php
@@ -4,13 +4,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Event_model extends CI_Model{
 
     function get_all_event(){
-        $this->db->select('event.id_event, event.id_admin, admin.nama_admin, event.nama_event,event.tanggal as tanggal, event.isi_event, event.gambar,kategori.nama_kategori,event.id_kategori,(select count(*) from komentar_forum kf where kf.id_forum = event.id_forum) as komen');
+        $this->db->select('event.id_event, event.id_admin, admin.nama_admin, event.nama_event,event.tanggal as tanggal, event.isi_event, event.gambar,kategori.nama_kategori,event.id_kategori,(select count(*) from komentar_event kf where kf.id_event = event.id_event) as komen');
         $this->db->from('event');
         $this->db->join('admin', 'event.id_admin = admin.id_admin');
         $this->db->join('kategori', 'event.id_kategori = kategori.id_kategori','left');
         //$this->db->order_by('id_event', 'DESC');
         $this->db->order_by('id_kategori');
         $hasil = $this->db->get()->result();
+        //echo $this->db->last_query();exit;
         return $hasil;
     }
 
